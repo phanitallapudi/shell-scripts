2dfa0311b4345bf2ef52d0ef8dcbf5ecc5b31cec
Panindhra <63370060+phanitallapudi@users.noreply.github.com>
2024-08-20 20:20:15 +0530

    Create install_python_in_linux.sh

---
+++ install_python_in_linux.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Update the package list
+sudo apt update
+
+# Install software-properties-common
+sudo apt install software-properties-common -y
+
+# Add the deadsnakes PPA for Python 3.11
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+
+# Update the package list again after adding the PPA
+sudo apt update
+
+# Install Python 3.11
+sudo apt install python3.11 -y
+
+# Verify the installation
+python3.11 --version
+
+# Install python3-venv if not installed
+sudo apt install python3-venv -y
