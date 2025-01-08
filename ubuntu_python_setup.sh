f92de7da9f666a850b6700b8563afd52f91bd6eb
Panindhra <63370060+phanitallapudi@users.noreply.github.com>
2025-01-08 17:08:13 +0530

    Update ubuntu_python_setup.sh

--- ubuntu_python_setup.sh
+++ ubuntu_python_setup.sh
@@ -21,6 +21,11 @@ sudo apt install python3.11-venv -y
 # Verify the installation of Python 3.11
 python3.11 --version
 
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
+
+# Verify the installation of Python 3.11
+python3 --version
+
 # Install python3-venv if not installed
 sudo apt install python3-venv -y
 
