17a5d7dc9ab9fdf86a24d58f16992222d75aec40
Panindhra <63370060+phanitallapudi@users.noreply.github.com>
2025-02-03 18:21:50 +0530

    Update ubuntu_docker_setup.sh

--- ubuntu_docker_setup.sh
+++ ubuntu_docker_setup.sh
@@ -33,4 +33,4 @@ echo "Docker installation completed!"
 docker --version
 sudo systemctl status docker --no-pager
 
-docker run -d --restart unless-stopped -p 60000:9000 --name portainer -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+sudo docker run -d --restart unless-stopped -p 60000:9000 --name portainer -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
